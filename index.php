43208b48ee347d4959916f4b14a521fe55385204
Lingcod <janedoooooe@gmail.com>
2014-05-05 00:04:26 -0400

    index page

--- index.php
+++ index.php
@@ -9,16 +9,29 @@ if(!$isGuest){
 
 <html>
 <head>
+<link type="text/css" rel="stylesheet" href="index.css">
 </head>
 <body>
-    <h1>Wilebook</h1>
-    <div>Welcome Guest!
-    <a href="login">Login</a> or <a href="signup">Sign up</a>
-
-<?php 
-    require 'login.php';
-    require 'signup.php';
-?>
-</div>
+    <div id="title">
+        <div id="logo">
+        <h1></h1>
+        </div>   
+    </div>
+        
+	<div id="welcome">
+        <div class="big_button" id="login"><a href="login">Login</a></div>
+         
+        <div class="big_button" id="signup"><a href="signup">Sign up</a></div>
+    </div>
+    
+    <?php 
+        require 'login.php';
+        require 'signup.php';
+    ?>
+    </div>
+    
+    <div id="aboutus">
+    <p>About us			Terms of use		Privacy Policy</p>
+    </div>
 </body>
 </html>
