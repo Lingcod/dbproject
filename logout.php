34eacb6f527d9a2de13558f5acae73fd8a2470d1
markwithk <markxu18@gmail.com>
2014-05-12 20:14:51 -0400

    recommend news for new user

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+    require_once 'utils.php';
+    $userid=$_SESSION['userid'];
+    $con->query("update user set lastaccess=NOW() where userid=$userid");
     session_destroy();
     header('Location: index');
 ?>
--- news.php
+++ news.php
@@ -31,14 +31,18 @@
     $divided=false;
     $result = mysqli_query($con,"select * from news natural join user where privacy<=getrelation(userid,$userid) and getrelation(userid,$userid) between 1 and 2 order by posttime desc ");
 
-
     if($result){
+	if($result->num_rows==0){
+	    $result = $con->query("select * from news natural join user where privacy=0");
+	    echo '<div  class="container"><div style="color: #5bc0de; text-align: center; font-weight: bold" >You do not have any news. So we recommend these to you.</div></div><br>';
+	}
+
 	while($row=$result->fetch_assoc()){
 		if($row['tablename']=='diary') $tablename='diary';
 		else if($row['tablename']=='actloc') $tablename='actloc';
 
 		if($row['posttime']<=$lastaccess && $divided==false ){
-		    echo '<div  class="container"><div style="color: #5bc0de; text-align: center; font-weight: bold" >Above is news before last time you login</div></div><br>';
+		    echo '<div  class="container"><div style="color: #5bc0de; text-align: center; font-weight: bold" >Above is what happended after last time you came</div></div><br>';
 
 		    $divided=true;
 		}
